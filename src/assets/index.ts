33e48304677af039205cbc62a70b42757088505c
bluesky995301-engine <bluesky995301@gmail.com>
2026-08-09 19:15:50 -0700

    update

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -2,7 +2,7 @@ import heroBg from './hero-bg.jpg'
 import heroVideo from './hero-video.webm'
 import aboutImage from './about.jpg'
 import ctaBg from './cta-bg.jpg'
-import logo from './logo.png'
+import logo from './logo-updaate.png'
 
 import client1 from './clients/client-1.png'
 import client2 from './clients/client-2.png'
